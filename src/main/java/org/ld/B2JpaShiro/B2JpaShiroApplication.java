f8abdf4758a7cddccf731172adfa2d613288c23d
xuesemofa@126.com <xuesemofa@126.com>
2018-07-06 15:42:16 +0800

    首次

---
+++ src/main/java/org/ld/B2JpaShiro/B2JpaShiroApplication.java
@@ -0,0 +1,12 @@
+package org.ld.B2JpaShiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2JpaShiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2JpaShiroApplication.class, args);
+	}
+}
